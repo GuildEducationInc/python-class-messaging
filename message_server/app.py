52d72bd906717673f0d0ef2c7824f25c70cbca85
valkolovos <val.kolovos@guildeducation.com>
2022-10-10 11:27:17 -0600

    adding database

--- message_server/app.py
+++ message_server/app.py
@@ -1,5 +1,7 @@
 # JSON is a lightweight format for storing and transporting data.
 import json
+# SQLite is a database that comes with Python
+import sqlite3
 
 # request is the way you access the incoming request
 # Flask is what we'll use to create the actual app instance
@@ -15,10 +17,6 @@ from collections import defaultdict
 # and isn't particularly relevant at the moment
 app = Flask(__name__)
 
-# Creating my storage. Note this will be empty every time I start the server. It would
-# be better to use a more permanent storage mechanism.
-MESSAGES=defaultdict(list)
-
 # This is a "decorator" (see https://realpython.com/primer-on-python-decorators/).
 # It tells the app we created above that when a request is received for this "path",
 # respond by using this function. It also contains a variable "user_id", which gets passed
@@ -26,8 +24,33 @@ MESSAGES=defaultdict(list)
 # in on the `user_id` variable to the function.
 @app.route('/get-messages/<user_id>')
 def get_messages(user_id):
-    # get messages from our storage variable for the given "user_id"
-    all_messages = MESSAGES.get(user_id, [])
+    # establish a connection to the database
+    conn = sqlite3.connect('messages.db')
+    # create a cursor which we will use to execute sql
+    cursor = conn.cursor()
+    # list to hold all messages
+    all_messages = []
+    try:
+        # execute sql to get messages for user
+        result = cursor.execute(
+            'SELECT * FROM messages WHERE to_user = ?',
+            (user_id,)
+        )
+        # get column names from response
+        column_names = [ descrip[0] for descrip in result.description ]
+        # build dictionary response
+        for msg in result.fetchall():
+            all_messages.append(
+                dict(
+                    (column_names[i], msg[i]) for i in range(len(column_names))
+                )
+            )
+    except Exception as e:
+        print(e.message)
+        return e.message, 500
+    finally:
+        cursor.close()
+        conn.close()
     # return the messages in a JSON format
     return json.dumps(all_messages)
 
@@ -46,8 +69,21 @@ def post_message():
        }
     '''
     message = json.loads(request.data)
-    # write to the MESSAGES storage variable using the "toUser" value from the message as the key
-    MESSAGES[message['toUser']].append(message)
+    conn = sqlite3.connect('messages.db')
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''
+          INSERT INTO messages (from_user, to_user, message)
+          VALUES (?, ?, ?)
+        ''', (message['fromUser'], message['toUser'], message['message'])
+        )
+        conn.commit()
+    except Exception as e:
+        print(e.message)
+        return e.message, 500
+    finally:
+        cursor.close()
+        conn.close()
     # return a nice response - for a comprehensive list of possible response codes,
     # see https://www.webfx.com/web-development/glossary/http-status-codes/
     return '', 202
